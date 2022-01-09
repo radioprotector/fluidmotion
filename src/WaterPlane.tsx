563ceef840d7cbc237ac5df87ebc1a6ccefd6cb0
Cate Stock <radioprotector@users.noreply.github.com>
2022-01-09 12:50:16 -0500

    move to double-buffered approach for geometry

--- src/WaterPlane.tsx
+++ src/WaterPlane.tsx
@@ -1,32 +1,31 @@
-import { useFrame } from "@react-three/fiber";
+import { ThreeEvent, useFrame } from "@react-three/fiber";
 import { useRef } from "react";
-import { PlaneGeometry, BufferGeometry, BufferAttribute, MathUtils } from "three";
+import { Mesh, PlaneGeometry, BufferGeometry, BufferAttribute, MathUtils } from "three";
 
 const NUM_ROWS = 128;
-const NUM_COLUMNS = 128; 
+const NUM_COLUMNS = 128;
+const MIN_Z_DEPTH = -1.0;
+const MAX_Z_DEPTH = 1.0;
+const BASE_Z_DEPTH = (MAX_Z_DEPTH - MIN_Z_DEPTH) / 2.0;
 
 function updateVertexColoring(geometry: BufferGeometry) {
   const vertexPositions = geometry.attributes.position;
   const vertexColors = geometry.attributes.color;
   const vertexCount = vertexPositions.count;
-  const MAX_DEPTH = 1.0;
-  const MIN_DEPTH = -1.0;
 
   for (let vertexIdx = 0; vertexIdx < vertexCount; vertexIdx++) {
     const vertexZ = vertexPositions.getZ(vertexIdx);
-    const colorScale = MathUtils.mapLinear(vertexZ, MIN_DEPTH, MAX_DEPTH, 0.0, 1.0);
+    const colorScale = MathUtils.mapLinear(vertexZ, MIN_Z_DEPTH, MAX_Z_DEPTH, 0.0, 1.0);
+
     vertexColors.setXYZ(vertexIdx, colorScale, colorScale, 1.0);
   }
 
   vertexColors.needsUpdate = true;
 }
 
-function updateVertexDepth(geometry: BufferGeometry, scratchPositions: BufferAttribute) {
-  const vertexPositions = geometry.attributes.position as BufferAttribute;
-  const vertexCount = vertexPositions.count;
-
-  // Ensure that the scratch buffer contains all of the existing positions
-  scratchPositions.copy(vertexPositions);
+// https://web.archive.org/web/20100224054436/http://www.gamedev.net/reference/programming/features/water/page2.asp
+function updateVertexDepth(sourcePositions: BufferAttribute, renderPositions: BufferAttribute) {
+  const vertexCount = sourcePositions.count;
 
   // Vertices are ordered like so:
   // [0 1 2]
@@ -42,45 +41,65 @@ function updateVertexDepth(geometry: BufferGeometry, scratchPositions: BufferAtt
 
     // Pull from the row above if possible
     if (rowIdx > 0) {
-      adjacentTotal += scratchPositions.getZ(vertexIdx - NUM_COLUMNS);
+      const aboveIdx = vertexIdx - NUM_COLUMNS;
+
+      adjacentTotal += sourcePositions.getZ(aboveIdx);
+
+      // // Also check upper-left and upper-right
+      // if (columnIdx > 0) {
+      //   adjacentTotal += sourcePositions.getZ(aboveIdx - 1);
+      // }
+
+      // if (columnIdx < NUM_COLUMNS - 1) {
+      //   adjacentTotal += sourcePositions.getZ(aboveIdx + 1);
+      // }
     }
 
     // Pull from the row below if possible
     if (rowIdx < NUM_ROWS - 1) {
-      adjacentTotal += scratchPositions.getZ(vertexIdx + NUM_COLUMNS);
+      const belowIdx = vertexIdx + NUM_COLUMNS;
+
+      adjacentTotal += sourcePositions.getZ(belowIdx);
+
+      // // Also check lower-left and lower-right
+      // if (columnIdx > 0) {
+      //   adjacentTotal += sourcePositions.getZ(belowIdx - 1);
+      // }
+      
+      // if (columnIdx < NUM_COLUMNS - 1) {
+      //   adjacentTotal += sourcePositions.getZ(belowIdx + 1);
+      // }
     }
 
     // Pull from the column on the left if possible
     if (columnIdx > 0) {
-      adjacentTotal += scratchPositions.getZ(vertexIdx - 1);
+      adjacentTotal += sourcePositions.getZ(vertexIdx - 1);
     }
 
     // Pull from the column on the right if possible
     if (columnIdx < NUM_COLUMNS - 1) {
-      adjacentTotal += scratchPositions.getZ(vertexIdx + 1);
+      adjacentTotal += sourcePositions.getZ(vertexIdx + 1);
     }
 
     // Take twice the average of the adjacent points and subtract it from the current position at this index
-    let newZValue = MathUtils.clamp((adjacentTotal / 2.0) - vertexPositions.getZ(vertexIdx), -1.0, 1.0);
+    let newZValue = MathUtils.clamp((adjacentTotal / 2.0) - renderPositions.getZ(vertexIdx), MIN_Z_DEPTH, MAX_Z_DEPTH);
 
     // Apply damping
-    newZValue *= 0.75;
+    newZValue *= 63/64;
 
     // Debug out-of-range values
     if (process.env.NODE_ENV !== 'production') {
       if (Number.isNaN(newZValue)) {
         console.debug(`NaN value for ${vertexIdx} at row ${rowIdx} col ${columnIdx}`);
-        newZValue = 0;
+        newZValue = BASE_Z_DEPTH;
       }
     }
 
-    vertexPositions.setZ(vertexIdx, newZValue);
+    renderPositions.setZ(vertexIdx, newZValue);
   }
   
   // Ensure this is updated
-  vertexPositions.needsUpdate = true;
-  geometry.computeBoundingBox();
-  geometry.computeVertexNormals();
+  renderPositions.needsUpdate = true;
 }
 
 // https://github.com/mrdoob/three.js/blob/master/examples/webgl_geometry_colors.html
@@ -89,17 +108,22 @@ function createWaterPlane(width: number, height: number): BufferGeometry {
   // Since the segments act as subdivisions, segment counts need to be 1 less than our goal.
   const baseGeometry = new PlaneGeometry(width, height, NUM_COLUMNS - 1, NUM_ROWS - 1);
 
-  // Set some default positions
+  // Set some default positions - everything but the center will be blank
   const vertexPositions = baseGeometry.attributes.position;
   const vertexCount = vertexPositions.count;
 
-  // Start at the center item
-  vertexPositions.setZ(Math.floor(vertexCount / 2 + (NUM_COLUMNS / 2)), 1.0);
+  for(let vertexIdx = 0; vertexIdx < vertexCount; vertexIdx++) {
+    vertexPositions.setZ(vertexIdx, BASE_Z_DEPTH);
+  }
 
-  console.debug(vertexPositions);
+  // // Set the center to a wave
+  // const centerVertexIdx = Math.floor(vertexCount / 2 + (NUM_COLUMNS / 2))
+  // vertexPositions.setZ(centerVertexIdx, MIN_Z_DEPTH);
+
+  // console.debug(vertexPositions);
 
   // Attach a color attribute and initialize its coloring
-  baseGeometry.setAttribute('color', new BufferAttribute( new Float32Array( vertexCount * 3 ), 3 ) );
+  baseGeometry.setAttribute('color', new BufferAttribute(new Float32Array(vertexCount * 3), 3));
   updateVertexColoring(baseGeometry);
 
   return baseGeometry;
@@ -107,21 +131,76 @@ function createWaterPlane(width: number, height: number): BufferGeometry {
 
 function WaterPlane(): JSX.Element {
   const waterGeometry = useRef(createWaterPlane(512, 512));
-  const scratchBuffer = useRef(waterGeometry.current.attributes.position.clone());
+  const sourceBuffer = useRef(waterGeometry.current.attributes.position as BufferAttribute);
+  const resultBuffer = useRef(waterGeometry.current.attributes.position.clone());
   const lastRenderTime = useRef(0);
-  const FRAME_SECONDS  = 1/20;
+  const waterMesh = useRef<Mesh>(null!);
+  const FRAME_SECONDS  = 1/30;
 
   useFrame((state) => {
     if (state.clock.elapsedTime > lastRenderTime.current + FRAME_SECONDS) {
-      updateVertexDepth(waterGeometry.current, scratchBuffer.current);
+      // Update the source and result buffer
+      updateVertexDepth(sourceBuffer.current, resultBuffer.current);
+      
+      // Ensure the two buffers get swapped
+      const temp = sourceBuffer.current;
+      sourceBuffer.current = resultBuffer.current;
+      resultBuffer.current = temp;
+      
+      // Ensure the geometry gets swapped and made up-to-date
+      waterGeometry.current.setAttribute("position", resultBuffer.current);
+      waterGeometry.current.computeVertexNormals();
+      waterGeometry.current.computeBoundingBox();
+      
+      // After the geometry is up to date, apply vertex coloring
       updateVertexColoring(waterGeometry.current);
+
       lastRenderTime.current = state.clock.elapsedTime;
     }
   });
 
+  const onPointerDown = (e: ThreeEvent<PointerEvent>): void => {
+    for(let intersection of e.intersections) {
+      // Make sure we're hitting the water mesh
+      if (e.object.uuid !== waterMesh.current.uuid) {
+        return;
+      }
+
+      // Get the intersection point in the object
+      const intersectionPointWorld = intersection.point;
+      const intersectionPointObject = intersection.object.worldToLocal(intersectionPointWorld);
+
+      // If we have a face, use that to easily find which of its vertices is closest to the click
+      if (intersection.face) {
+        let nearestVertexIdx = -1;
+        let nearestVertexDistance = Number.MAX_SAFE_INTEGER;
+
+        [intersection.face.a, intersection.face.b, intersection.face.c].forEach((vertexIdx) => {
+          // Get the X/Y coordinates of this vertex
+          const vertexPositionX = sourceBuffer.current.getX(vertexIdx);
+          const vertexPositionY = sourceBuffer.current.getY(vertexIdx);
+
+          // Get the distance to the click X/Y coordinates in object space
+          let distanceFromIntersection = Math.sqrt(Math.pow(vertexPositionX - intersectionPointObject.x, 2) + Math.pow(vertexPositionY - intersectionPointObject.y, 2));
+          
+          if (distanceFromIntersection < nearestVertexDistance) {
+            nearestVertexIdx = vertexIdx;
+            nearestVertexDistance = distanceFromIntersection;
+          }
+        });
+
+        // Now update the source buffer at this index to reflect the depth change
+        sourceBuffer.current.setZ(nearestVertexIdx, MAX_Z_DEPTH);
+        e.stopPropagation();
+      }
+    }
+  };
+
   return (
     <mesh
+      ref={waterMesh}
       position={[0, 0, -256]}
+      onPointerDown={onPointerDown}
     >
       <primitive object={waterGeometry.current} attach="geometry" />
       <meshPhongMaterial color={0x7777ff} flatShading={true} shininess={0} vertexColors={true} />
