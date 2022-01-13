81bfcfa1a33b823a5fd1d17b1f56d530568fb431
Cate Stock <radioprotector@users.noreply.github.com>
2022-01-13 02:15:33 -0500

    start to split plane across multiple meshes

--- src/WaterPlane.tsx
+++ src/WaterPlane.tsx
@@ -1,8 +1,52 @@
 import { ThreeEvent, useFrame } from "@react-three/fiber";
-import { useRef } from "react";
-import { Mesh, PlaneGeometry, BufferGeometry, BufferAttribute, MathUtils } from "three";
+import { useRef, useMemo } from "react";
+import { Mesh, PlaneGeometry, BufferGeometry, BufferAttribute, MathUtils, MeshPhongMaterial } from "three";
 
-// These two values control the number of vertices that will be in the plane, e.g.:
+/**
+ * Describes a subdivision of the water plane. Used to help reduce overhead for intersection hit testing.
+ */
+interface WaterPlaneSubdivision {
+  /**
+   * The unique key for the subdivision.
+   */
+  key: string;
+
+  /**
+   * The row index of this subdivision.
+   */
+  rowIndex: number;
+
+  /**
+   * The column index of this subdivision.
+   */
+  columnIndex: number;
+
+  /**
+   * The mesh for the subdivision.
+   */
+  mesh: Mesh;
+
+  /**
+   * The geometry in use for the mesh.
+   */
+  geometry: BufferGeometry;
+
+  /**
+   * The buffer attribute containing source vertex positions.
+   */
+  sourcePositions: BufferAttribute;
+
+  /**
+   * The buffer attribute containing resulting vertex positions after applying propagation logic to the source positions.
+   */
+  resultPositions: BufferAttribute;
+}
+
+// These two values control how many subdivisions will be used for the water plane.
+const SUBDIVISION_ROWS = 1;
+const SUBDIVISION_COLUMNS = 1;
+
+// These two values control the number of vertices that will be in each plane subdivision, e.g.:
 // 256x256 will result in 65,536 vertices
 // 512x512 will result in 262,144 vertices
 const NUM_ROWS = 256;
@@ -13,6 +57,21 @@ const MIN_Z_DEPTH = -1.0;
 const MAX_Z_DEPTH = 1.0;
 const BASE_Z_DEPTH = (MAX_Z_DEPTH + MIN_Z_DEPTH) / 2.0;
 const WAVE_DAMPING = 127/128;
+
+/**
+ * The material to use for the water.
+ */
+ const WaterMaterial = new MeshPhongMaterial({color: 0x7777ff, flatShading: true, shininess: 1.0, vertexColors: true});
+
+/**
+ * Calculates a subdivision key for the given row/column index.
+ * @param rowIndex The row index.
+ * @param columnIndex The column index.
+ * @returns The corresponding key.
+ */
+function getSubdivisionKey(rowIndex: number, columnIndex: number): string {
+  return `sub${rowIndex}-${columnIndex}`;
+}
 
 /**
  * Applies coloring to each vertex in the geometry based on its z-depth.
@@ -34,13 +93,12 @@ function updateVertexColoring(geometry: BufferGeometry) {
 }
 
 /**
- * Updates the z-depth of all vertices in the render buffer attribute to reflect its adjacent vertices.
- * @param sourcePositions The buffer attribute of vertex positions that will be used as sources.
- * @param renderPositions The buffer attribute of vertex positions that will be rendered.
+ * Updates the z-depth of vertices in the provided subdivision.
+ * @param subdivision The subdivision to update.
  * @see {@link https://web.archive.org/web/20100224054436/http://www.gamedev.net/reference/programming/features/water/page2.asp}
  */
-function updateVertexDepth(sourcePositions: BufferAttribute, renderPositions: BufferAttribute) {
-  const vertexCount = sourcePositions.count;
+function updateVertexDepth(subdivision: WaterPlaneSubdivision) {
+  const vertexCount = subdivision.sourcePositions.count;
 
   // Vertices are ordered like so:
   // [0 1 2]
@@ -58,28 +116,28 @@ function updateVertexDepth(sourcePositions: BufferAttribute, renderPositions: Bu
     if (rowIdx > 0) {
       const aboveIdx = vertexIdx - NUM_COLUMNS;
 
-      adjacentTotal += sourcePositions.getZ(aboveIdx);
+      adjacentTotal += subdivision.sourcePositions.getZ(aboveIdx);
     }
 
     // Pull from the row below if possible
     if (rowIdx < NUM_ROWS - 1) {
       const belowIdx = vertexIdx + NUM_COLUMNS;
 
-      adjacentTotal += sourcePositions.getZ(belowIdx);
+      adjacentTotal += subdivision.sourcePositions.getZ(belowIdx);
     }
 
     // Pull from the column on the left if possible
     if (columnIdx > 0) {
-      adjacentTotal += sourcePositions.getZ(vertexIdx - 1);
+      adjacentTotal += subdivision.sourcePositions.getZ(vertexIdx - 1);
     }
 
     // Pull from the column on the right if possible
     if (columnIdx < NUM_COLUMNS - 1) {
-      adjacentTotal += sourcePositions.getZ(vertexIdx + 1);
+      adjacentTotal += subdivision.sourcePositions.getZ(vertexIdx + 1);
     }
 
     // Take twice the average of the adjacent points and subtract it from the current position at this index
-    let newZValue = MathUtils.clamp((adjacentTotal / 2.0) - renderPositions.getZ(vertexIdx), MIN_Z_DEPTH, MAX_Z_DEPTH);
+    let newZValue = MathUtils.clamp((adjacentTotal / 2.0) - subdivision.resultPositions.getZ(vertexIdx), MIN_Z_DEPTH, MAX_Z_DEPTH);
 
     // Apply damping
     newZValue *= WAVE_DAMPING;
@@ -92,11 +150,11 @@ function updateVertexDepth(sourcePositions: BufferAttribute, renderPositions: Bu
       }
     }
 
-    renderPositions.setZ(vertexIdx, newZValue);
+    subdivision.resultPositions.setZ(vertexIdx, newZValue);
   }
   
   // Ensure this is updated
-  renderPositions.needsUpdate = true;
+  subdivision.resultPositions.needsUpdate = true;
 }
 
 /**
@@ -126,22 +184,86 @@ function createWaterPlane(width: number, height: number): BufferGeometry {
   return baseGeometry;
 }
 
+/**
+ * Creates and returns the subdivisions for the plane.
+ * @param totalPlaneDimensions The width and height of the plane as a whole.
+ */
+function createSubdivisions(totalPlaneDimensions: number): WaterPlaneSubdivision[] {
+  const INITIAL_OFFSET = -totalPlaneDimensions / 2;
+  const PLANE_HEIGHT = totalPlaneDimensions / SUBDIVISION_ROWS;
+  const PLANE_WIDTH = totalPlaneDimensions / SUBDIVISION_COLUMNS;
+  const subdivisions: WaterPlaneSubdivision[] = [];
+
+  for(let rowIdx = 0; rowIdx < SUBDIVISION_ROWS; rowIdx++) {
+    let rowVertOffset = INITIAL_OFFSET + (0.5 * PLANE_HEIGHT * ((rowIdx + 1) / SUBDIVISION_ROWS));
+
+    for(let colIdx = 0; colIdx < SUBDIVISION_COLUMNS; colIdx++) {
+      let colHorizOffset = INITIAL_OFFSET + (0.5 * PLANE_WIDTH * ((colIdx + 1) / SUBDIVISION_COLUMNS));
+
+      // Start by creating the plane
+      const waterGeometry = createWaterPlane(PLANE_HEIGHT, PLANE_WIDTH);
+
+      // Also attach a mesh and position it
+      const waterMesh = new Mesh(waterGeometry, WaterMaterial);
+      waterMesh.position.set(colHorizOffset, rowVertOffset, -256);
+
+      const subdivision: WaterPlaneSubdivision = {
+        key: getSubdivisionKey(rowIdx, colIdx),
+        rowIndex: rowIdx,
+        columnIndex: colIdx,
+        mesh: waterMesh,
+        geometry: waterGeometry,
+        sourcePositions: waterGeometry.attributes.position as BufferAttribute,
+        resultPositions: waterGeometry.attributes.position.clone()
+      };
+
+      subdivisions.push(subdivision);
+    }
+  }
+
+  return subdivisions;
+}
+
 function WaterPlane(): JSX.Element {
   const PLANE_DIMENSIONS = 4096;
-  const waterMesh = useRef<Mesh>(null!);
-  const waterGeometry = useRef(createWaterPlane(PLANE_DIMENSIONS, PLANE_DIMENSIONS));
-
-  // Create two different position buffers to swap with each render/wave propagation pass
-  // https://web.archive.org/web/20100224054436/http://www.gamedev.net/reference/programming/features/water/page2.asp
-  const sourceBuffer = useRef(waterGeometry.current.attributes.position as BufferAttribute);
-  const resultBuffer = useRef(waterGeometry.current.attributes.position.clone());
 
   // Track when we last updated the buffers
   const lastRenderTime = useRef(0);
   const FRAME_SECONDS = 1/30;
 
   // Track what's being pointed at
+  const pointerSubdivisionRowIndex = useRef(-1);
+  const pointerSubdivisionColumnIndex = useRef(-1);
   const pointerVertexIndex = useRef(-1);
+
+  // Build subdivisions, using a ref to maintain between refreshes
+  const subdivisions = useRef(createSubdivisions(PLANE_DIMENSIONS));
+
+  // Build other versions of this using a memoized format
+  const subdivisionsByUuid: Record<string, WaterPlaneSubdivision> = useMemo(() => {
+    const uuidMap: Record<string, WaterPlaneSubdivision> = {};
+
+    subdivisions.current.forEach((sub) => {
+      uuidMap[sub.mesh.uuid] = sub;
+    });
+
+    return uuidMap;
+  }, [subdivisions])
+
+  const subdivisionsByRowCol: WaterPlaneSubdivision[][] = useMemo(() => {
+    const rowColArr: WaterPlaneSubdivision[][] = [];
+
+    subdivisions.current.forEach((sub) => {
+      // Make sure we have an array defined for the row
+      if (rowColArr[sub.rowIndex] === undefined) {
+        rowColArr[sub.rowIndex] = [];
+      }
+
+      rowColArr[sub.rowIndex][sub.columnIndex] = sub;
+    })
+
+    return rowColArr;
+  }, [subdivisions]);
 
   const setCurrentPointer = (e: ThreeEvent<PointerEvent>): void => {
     // If we're only moving, don't bother unless the pointer is down
@@ -151,10 +273,13 @@ function WaterPlane(): JSX.Element {
 
     // Now look for intersections
     for(let intersection of e.intersections) {
-      // Make sure we're hitting the water mesh
-      if (e.object.uuid !== waterMesh.current.uuid) {
+      // Make sure we're hitting a subdivision
+      if (e.object.uuid in subdivisionsByUuid === false) {
         continue;
       }
+
+      // Get the subdivision
+      const subdivision = subdivisionsByUuid[e.object.uuid];
 
       // Get the intersection point in the object
       const intersectionPointWorld = intersection.point;
@@ -167,8 +292,8 @@ function WaterPlane(): JSX.Element {
 
         [intersection.face.a, intersection.face.b, intersection.face.c].forEach((vertexIdx) => {
           // Get the X/Y coordinates of this vertex
-          const vertexPositionX = sourceBuffer.current.getX(vertexIdx);
-          const vertexPositionY = sourceBuffer.current.getY(vertexIdx);
+          const vertexPositionX = subdivision.sourcePositions.getX(vertexIdx);
+          const vertexPositionY = subdivision.sourcePositions.getY(vertexIdx);
 
           // Get the distance to the click X/Y coordinates in object space
           let distanceFromIntersection = Math.sqrt(Math.pow(vertexPositionX - intersectionPointObject.x, 2) + Math.pow(vertexPositionY - intersectionPointObject.y, 2));
@@ -181,6 +306,8 @@ function WaterPlane(): JSX.Element {
 
         // Set the pointer that we're at and exit out
         pointerVertexIndex.current = nearestVertexIdx;
+        pointerSubdivisionRowIndex.current = subdivision.rowIndex;
+        pointerSubdivisionColumnIndex.current = subdivision.columnIndex;
         e.stopPropagation();
         return;
       }
@@ -188,69 +315,69 @@ function WaterPlane(): JSX.Element {
   };
 
   const clearCurrentPointer = (e: ThreeEvent<PointerEvent>): void => {
+    pointerSubdivisionRowIndex.current = -1;
+    pointerSubdivisionColumnIndex.current = -1;
     pointerVertexIndex.current = -1;
   };
 
   useFrame((state) => {
     // See if we have pointer data to apply - if so, explicitly set the z-value
     // FUTURE: Look at debouncing this if necessary
-    if (pointerVertexIndex.current > -1) {
-      sourceBuffer.current.setZ(pointerVertexIndex.current, MIN_Z_DEPTH);
+    if (pointerVertexIndex.current > -1 && pointerSubdivisionRowIndex.current > -1 && pointerSubdivisionColumnIndex.current > -1) {
+      const subdivision = subdivisionsByRowCol[pointerSubdivisionRowIndex.current][pointerSubdivisionColumnIndex.current];
+      subdivision.sourcePositions.setZ(pointerVertexIndex.current, MIN_Z_DEPTH);
     }
 
-    // Determine the constraints of the screen and scale to them
-    const screenWidth = window.innerWidth || document.documentElement.clientWidth;
-    const screenHeight = window.innerHeight || document.documentElement.clientHeight;
-    const maxDimension = Math.max(screenWidth, screenHeight);
-    const scaleToMaxDimension = maxDimension / PLANE_DIMENSIONS;
+    // // Determine the constraints of the screen and scale to them
+    // const screenWidth = window.innerWidth || document.documentElement.clientWidth;
+    // const screenHeight = window.innerHeight || document.documentElement.clientHeight;
+    // const maxDimension = Math.max(screenWidth, screenHeight);
+    // const scaleToMaxDimension = maxDimension / PLANE_DIMENSIONS;
 
-    if (waterMesh.current.scale.x !== scaleToMaxDimension) {
-      console.debug(`scaling ${PLANE_DIMENSIONS}x${PLANE_DIMENSIONS} to ${screenWidth}x${screenHeight} w/ ${scaleToMaxDimension.toFixed(2)}`)
-      waterMesh.current.scale.set(scaleToMaxDimension, scaleToMaxDimension, 1);
-    }
+    // if (waterMesh.current.scale.x !== scaleToMaxDimension) {
+    //   console.debug(`scaling ${PLANE_DIMENSIONS}x${PLANE_DIMENSIONS} to ${screenWidth}x${screenHeight} w/ ${scaleToMaxDimension.toFixed(2)}`)
+    //   waterMesh.current.scale.set(scaleToMaxDimension, scaleToMaxDimension, 1);
+    // }
     
     // See if it's time to update the buffers
     if (state.clock.elapsedTime > lastRenderTime.current + FRAME_SECONDS) {
-      // Update the source and result position buffer
-      updateVertexDepth(sourceBuffer.current, resultBuffer.current);
+
+      // Update each subdivision
+      for (let subdivision of subdivisions.current) {
+        // Update the source and render position buffers
+        updateVertexDepth(subdivision);
+
+        // Ensure the two position buffers get swapped
+        const temp = subdivision.sourcePositions;
+        subdivision.sourcePositions = subdivision.resultPositions;
+        subdivision.resultPositions = temp;
       
-      // Ensure the two position buffers get swapped
-      const temp = sourceBuffer.current;
-      sourceBuffer.current = resultBuffer.current;
-      resultBuffer.current = temp;
-      
-      // Ensure the geometry uses the new position attribute set and recomputed
-      waterGeometry.current.setAttribute("position", resultBuffer.current);
-      waterGeometry.current.computeVertexNormals();
-      waterGeometry.current.computeBoundingBox();
-      
-      // After the geometry is up to date, apply vertex coloring
-      updateVertexColoring(waterGeometry.current);
+        // Ensure the geometry uses the new position attribute set and recomputed normals/sphere
+        subdivision.geometry.setAttribute("position", subdivision.resultPositions);
+        subdivision.geometry.computeVertexNormals();
+        subdivision.geometry.computeBoundingSphere();
+        
+        // After the geometry is up to date, apply vertex coloring
+        updateVertexColoring(subdivision.geometry);
+      }
 
       lastRenderTime.current = state.clock.elapsedTime;
     }
   });
 
   return (
-    <mesh
-      ref={waterMesh}
-      position={[0, 0, -256]}
-      onPointerDown={setCurrentPointer}
-      onPointerMove={setCurrentPointer}
-      onPointerUp={clearCurrentPointer}
-      onPointerLeave={clearCurrentPointer}
-    >
-      <primitive
-        object={waterGeometry.current}
-        attach="geometry"
-      />
-      <meshPhongMaterial
-        color={0x7777ff}
-        flatShading={true}
-        shininess={1}
-        vertexColors={true}
-      />
-    </mesh>
+    <group>
+      {subdivisions.current.map((subdivision) => {
+        return <primitive
+          object={subdivision.mesh}
+          key={subdivision.key}
+          onPointerDown={setCurrentPointer}
+          onPointerMove={setCurrentPointer}
+          onPointerUp={clearCurrentPointer}
+          onPointerLeave={clearCurrentPointer}
+          />;
+      })}
+    </group>
   );
 }
 
